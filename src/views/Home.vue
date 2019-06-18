32630e5b8f8f4774452c5b2d3138a33ed40dd6f0
Marx JMoura <marx.jmoura@gmail.com>
2019-06-18 20:32:57 -0300

    Create Vue.js project

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img alt="Logo da ufollow" src="https://ufollow.io/img/u.svg" height="128">
+    <HelloWorld msg="Bem-vindo a ufollow :)"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
