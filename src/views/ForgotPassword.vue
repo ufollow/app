f17d33b44524b75ef26b4bc430ce36952a9964ba
Marx JMoura <marx.jmoura@gmail.com>
2019-06-19 12:41:54 -0300

    Add pages for reset password

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -5,36 +5,31 @@
         <div class="card o-hidden border-0 shadow-lg my-5" style="flex: 1">
           <div class="card-body p-0">
             <div class="row h-100">
-              <div class="col-lg-6 d-none d-lg-block bg-signin"></div>
+              <div class="col-lg-6 d-none d-lg-block bg-forgot-password"></div>
               <div class="col-lg-6 h-100 d-flex flex-column justify-content-center">
                 <div class="p-5">
-                  <div class="text-center">
+                  <div class="text-center mb-4">
                     <img src="https://ufollow.io/img/u.svg" alt="Logo da ufollow" class="mb-4" width="48">
                     <h1 class="display-3 text-gray-900 mb-4">
-                      Bem-vindo(a) de volta!
+                      Esqueceu a senha?
                     </h1>
+                    <p>
+                      Tudo bem. Informe seu e-mail que te enviaremos
+                      um código para você definir uma nova senha.
+                    </p>
                   </div>
                   <form>
                     <div class="form-group">
                       <input type="email" class="form-control rounded-pill p-4" placeholder="E-mail">
                     </div>
-                    <div class="form-group">
-                      <input type="password" class="form-control rounded-pill p-4" placeholder="Senha">
-                    </div>
-                    <a href="index.html" class="btn btn-sm btn-primary btn-block rounded-pill py-3 text-uppercase">
-                      Login
-                    </a>
+                    <router-link :to="{ name: 'resetPassword' }" class="btn btn-sm btn-primary btn-block rounded-pill py-3 text-uppercase">
+                      Enviar código
+                    </router-link>
                   </form>
                   <hr>
-                  <div class="text-center mb-5">
-                    Não lembra sua senha?<br>
-                    <router-link :to="{ name: 'forgotPassword' }">
-                      Clique aqui para definir uma nova
-                    </router-link>
-                  </div>
-                  <div class="text-center">
-                    <router-link :to="{ name: 'signup' }">
-                      Criar uma conta
+                  <div class="text-center mt-5">
+                    <router-link :to="{ name: 'signin' }">
+                      Voltar
                     </router-link>
                   </div>
                 </div>
@@ -49,6 +44,6 @@
 
 <script>
 export default {
-  name: 'signin'
+  name: 'forgotPassword'
 }
 </script>
