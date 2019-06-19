931120e30c650ada0d2746df1f68879ab48e4986
Marx JMoura <marx.jmoura@gmail.com>
2019-06-19 08:41:46 -0300

    Add signup page

--- src/router.js
+++ src/router.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Signin from './views/Signin.vue'
+import Signup from './views/Signup.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'signin',
-      component: Signin
-    }
+    { path: '/', name: 'signin', component: Signin },
+    { path: '/criar-conta', name: 'signup', component: Signup }
   ]
 })
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -5,16 +5,19 @@
         <div class="card o-hidden border-0 shadow-lg my-5" style="flex: 1">
           <div class="card-body p-0">
             <div class="row h-100">
-              <div class="col-lg-6 d-none d-lg-block bg-signin"></div>
+              <div class="col-lg-6 d-none d-lg-block bg-signup"></div>
               <div class="col-lg-6 h-100 d-flex flex-column justify-content-center">
                 <div class="p-5">
                   <div class="text-center">
                     <img src="https://ufollow.io/img/u.svg" alt="Logo da ufollow" class="mb-4" width="48">
                     <h1 class="display-3 text-gray-900 mb-4">
-                      Bem-vindo(a) de volta!
+                      Seja bem-vindo(a)!
                     </h1>
                   </div>
                   <form>
+                    <div class="form-group">
+                      <input type="text" class="form-control rounded-pill p-4" placeholder="Seu nome">
+                    </div>
                     <div class="form-group">
                       <input type="email" class="form-control rounded-pill p-4" placeholder="E-mail">
                     </div>
@@ -22,19 +25,17 @@
                       <input type="password" class="form-control rounded-pill p-4" placeholder="Senha">
                     </div>
                     <a href="index.html" class="btn btn-sm btn-primary btn-block rounded-pill py-3 text-uppercase">
-                      Login
+                      Criar conta
                     </a>
                   </form>
                   <hr>
                   <div class="text-center mb-5">
-                    Não lembra sua senha?<br>
-                    <a href="#esqueci-a-senha">
-                      Clique aqui para definir uma nova
-                    </a>
+                    Ao criar uma conta você concorda com os nossos<br>
+                    <a href="#termos-de-uso">Termos de uso</a>
                   </div>
                   <div class="text-center">
-                    <router-link :to="{ name: 'signup' }">
-                      Criar uma conta
+                    <router-link :to="{ name: 'signin' }">
+                      Já tenho uma conta
                     </router-link>
                   </div>
                 </div>
@@ -49,6 +50,6 @@
 
 <script>
 export default {
-  name: 'signin'
+  name: 'signup'
 }
 </script>
